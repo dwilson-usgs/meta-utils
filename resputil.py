fixed a0 calculation

resputil.py
@@ -49,7 +49,7 @@ def get_unit(resp):
             unit = key
     return unit
 
-def calc_a0(resp,f):
+def old_calc_a0(resp,f):  # This one fails if gain and normalization frequencies are not the same
     unit=get_unit(resp)
     pz=resp.get_paz()
     #resp.response_stages[0].stage_gain=1
@@ -59,21 +59,36 @@ def calc_a0(resp,f):
     a0 = 1/a1
     return a0
 
+def calc_a0(resp,f):
+    pz=resp.get_paz()
+    omega=2*np.pi*f
+    a1=1
+    for n in range(len(pz.zeros)):
+        a1=a1*(1j*omega - pz.zeros[n])
+    for n in range(len(pz.poles)):
+        a1=a1/(1j*omega - pz.poles[n])
+    a0= 1/np.abs(a1)
+    return a0
+
 ###############
 with warnings.catch_warnings():
     warnings.filterwarnings(action='ignore')
     resp1=myresp[0][0][0].response
     pz=resp1.get_paz()
-    if freq>998:
-        freq=pz.normalization_frequency
-    a0=calc_a0(dcopy(resp1),freq)
     a1=resp1.instrument_sensitivity.value
     a1f=resp1.instrument_sensitivity.frequency
-    #resp1.recalculate_overall_sensitivity(frequency=freq)
-    #a2=resp1.instrument_sensitivity.value * a0 / pz.normalization_factor
-    a2=np.abs(resp1.get_evalresp_response_for_frequencies([freq],output=get_unit(resp1))) * a0 / pz.normalization_factor
+    if freq>998:
+        freq=pz.normalization_frequency
+        f2=a1f
+    else:
+        f2=freq
+        
+    a2=np.abs(resp1.get_evalresp_response_for_frequencies([f2],output=get_unit(resp1)))
+    a0=calc_a0(dcopy(resp1),freq)
+    
+    
 
     print("\n a0 Stated: %13.6E at %3.2f Hz, Computed: %13.6E at %3.2f Hz \n"%(pz.normalization_factor,pz.normalization_frequency,a0,freq))
-    print("Overall Sensitivity Stated: %13.6E at %3.2f Hz, Computed: %13.6E at %3.2f Hz"%(a1,a1f,a2,freq))
+    print("Overall Sensitivity Stated: %13.6E at %3.2f Hz, Computed: %13.6E at %3.2f Hz"%(a1,a1f,a2,f2))
          
             